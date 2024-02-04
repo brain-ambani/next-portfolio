809defddbb50a353e68746801eb1fd1956717011
brian-ambani <brainambani1@gmail.com>
2024-02-04 14:15:56 +0300

    updated the folde structure

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Brian | Portfolio",
+  description:
+    "Brian is a full-stack developer based in the Kenya. He specializes in building web applications using modern technologies.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Personal Portfolio</h1>
+    </main>
+  );
+}
