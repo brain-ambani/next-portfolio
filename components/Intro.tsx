908571511a643b427d258da215ffefb99c15699e
Brian-Ambani <brainambani1@gmail.com>
2024-02-18 04:32:34 +0300

    added the intro section for all devices

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,22 +1,100 @@
+"use client";
+
 import Image from "next/image";
 import profilePic from "../public/rmtdev.png";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithub } from "react-icons/fa";
 
 export const Intro = () => {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
-        <div>
-          <Image
-            src={profilePic}
-            alt="profile photo"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true}
-            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-          />
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={profilePic}
+              alt="profile photo"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+            <motion.span
+              className="absolute text-4xl bottom-0 right-0"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 125,
+                delay: 0.1,
+                duration: 0.2,
+              }}
+            >
+              👋
+            </motion.span>
+          </motion.div>
         </div>
       </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Brain.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">1.5 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-5 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group px-7 py-3 text-lg font-medium text-white bg-gray-900 flex items-center  gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacty-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          href="/rmtdev.png"
+          download
+          className="group bg-white px-7 py-3 text-lg font-medium flex items-center  gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/brain-ambani/"
+          target="_blank"
+          rel="noreferrer"
+          className="bg-white text-gray-700 text-[2rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/brian-ambani"
+          target="_blank"
+          rel="noreferrer"
+          className="bg-white text-gray-700 text-[2rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithub />
+        </a>
+      </motion.div>
     </section>
   );
 };
