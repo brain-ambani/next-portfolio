cebc95afc72a5e99442e23841a896bfaab10280d
Brian-Ambani <brainambani1@gmail.com>
2024-02-19 13:23:22 +0300

    add the about component

---
+++ components/SectionHeading.tsx
@@ -0,0 +1,7 @@
+type sectionHeadingProps = {
+  children: React.ReactNode;
+};
+
+export default function SectionHeading({ children }: sectionHeadingProps) {
+  return <h2 className="text-3xl font-medium mb-8">{children}</h2>;
+}
