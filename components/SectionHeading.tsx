79b0914fbb0795243a0765b448e40cac36ec235a
Brian-Ambani <brainambani1@gmail.com>
2024-03-07 00:45:29 +0300

    remove  the alternating image and project description on the project section

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -3,5 +3,9 @@ type sectionHeadingProps = {
 };
 
 export default function SectionHeading({ children }: sectionHeadingProps) {
-  return <h2 className="text-3xl font-medium mb-8">{children}</h2>;
+  return (
+    <h2 className="text-3xl font-medium mb-8 capitalize text-center">
+      {children}
+    </h2>
+  );
 }
