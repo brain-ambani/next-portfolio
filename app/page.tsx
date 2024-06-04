e0a81d3746a606806c6e8bf0133d8deec91623a9
brian-ambani <brainambani1@gmail.com>
2024-06-04 22:14:39 +0300

    added the projects titles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import Grid from "../components/Grid";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
         />
         <Hero />
         <Grid />
+        <Projects />
       </div>
     </main>
   );
