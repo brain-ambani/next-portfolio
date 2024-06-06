11f4c03c4db7c46c623680426841cddb8879c995
brian-ambani <brainambani1@gmail.com>
2024-06-06 23:28:52 +0300

    updated the nav

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "../components/Grid";
 import Projects from "@/components/Projects";
+import Clients from "@/components/Clients";
 import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <Projects />
+        <Clients />
         <Footer />
       </div>
     </main>
