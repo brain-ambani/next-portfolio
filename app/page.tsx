fbbe9db86de5de2964dac575b8e6eddf5fd011e8
brian-ambani <brainambani1@gmail.com>
2024-06-06 23:20:18 +0300

    updated footer and clients components

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import Grid from "../components/Grid";
 import Projects from "@/components/Projects";
+import Footer from "@/components/Footer";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <Projects />
+        <Footer />
       </div>
     </main>
   );
