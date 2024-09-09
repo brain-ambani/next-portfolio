930e9e1f0fba34363e24620f98d1878a08d6affb
brain ambani <brainambani1@gmail.com>
2024-09-09 20:46:12 +0300

    updated the project image

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -9,7 +9,6 @@ export const ProjectCard = () => {
     <div className="">
       <p className="text-cyan-400 font-light">Recent Projects</p>
       <ul>
-        return(
         {projects.map((project, index) => (
           <div key={index} className="py-12 sm:py-8 ">
             <li className="font-bold text-xl text-slate-50 py-2">
@@ -18,17 +17,21 @@ export const ProjectCard = () => {
 
             <div className="relative ">
               <div className="relative flex items-center bg-[#0A122A] sm:justify-end sm:p-8 rounded-md">
-                <p className=" absolute z-10 left-8 text-sm  sm:top-1/2 sm:transform sm:-translate-y-1/2 sm:w-[50%]  sm:bg-[#00091d] p-2 rounded-md shadow-inner  ">
-                  {project.description}
-                </p>
+                <div>
+                  <p className=" absolute z-10 left-8 text-sm  sm:top-1/2 sm:transform sm:-translate-y-1/2 sm:w-[50%]  sm:bg-[#00091d] p-2 rounded-md shadow-inner  ">
+                    {project.description}
+                  </p>
+                </div>
 
-                <Image
-                  src={project.image}
-                  width={550}
-                  height={200}
-                  alt="project img"
-                  className="object-cover w-[550px] h-[250px] rounded-md sm:opacity-65 hover:scale-105 hover:opacity-75 transition opacity-20"
-                />
+                <div>
+                  <Image
+                    src={project.image}
+                    width={550}
+                    height={200}
+                    alt={project.title}
+                    className="object-cover w-[550px] h-[250px] rounded-md sm:opacity-65 hover:scale-105 hover:opacity-75 transition opacity-20"
+                  />
+                </div>
               </div>
               <div className=" absolute sm:bottom-3 -bottom-16">
                 <div className=" sm:block my-2 text-xs sm:text-sm text-cyan-200 sm:pl-8">
