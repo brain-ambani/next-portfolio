c31c1fc48f5fdfe6be6d4bd211ef6b533db03b16
brain ambani <brainambani1@gmail.com>
2024-09-10 07:18:12 +0300

    updated the mobile responsivenes of the profile image on sm devices

--- components/about-display.tsx
+++ components/about-display.tsx
@@ -9,9 +9,9 @@ export const AboutDisplay = () => {
           width={350}
           height={350}
           alt="about"
-          className=" rounded-md z-2 opacity-95 "
+          className=" rounded-md z-2 opacity-95 w-[300px] h-[300px] sm:w-[350px] sm:h-[350px]"
         />
-        <div className="absolute  top-3 left-3 sm:top-4 sm:left-4 w-[320px] h-[320px] sm:w-[350px] sm:h-[350px]  rounded-md border-r-2 border-b-2 border-l-0 border-cyan-400 hover:top-1 hover:left-1 sm:hover:top-2 sm:hover:left-2  transition-all duration-300 ease-in-out z-1"></div>
+        <div className="absolute  top-3 left-3 sm:top-4 sm:left-4 w-[300px] h-[300px] sm:w-[350px] sm:h-[350px]  rounded-md border-r-2 border-b-2 border-l-0 border-cyan-400 hover:top-1 hover:left-1 sm:hover:top-2 sm:hover:left-2  transition-all duration-300 ease-in-out z-1"></div>
       </div>
     </div>
   );
